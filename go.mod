6ec3927ecc0f8f8884184644ffc9eb453078aef7
adi saputra <adii.empty@gmail.com>
2022-03-06 21:52:37 +0800

    tambah module dari say_hello

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/wynadiis23/app-say-hello.git
+
+go 1.15
+
+require github.com/wynadiis23/go-say-hello/v2 v2.0.0
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/wynadiis23/go-say-hello/v2"
+)
+
+func main() {
+
+	fmt.Println(go_say_hello.SayHello("adi"))
+}
